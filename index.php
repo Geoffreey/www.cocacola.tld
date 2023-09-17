c5dccc509da96bd407d9b89b0e5d936f4ca475b9
Geoffreey <geoffrey.torres64@gmail.com>
2023-09-16 22:17:24 -0600

    Se cambio extension de archivo para agregar envio de formulario poro correo mediante PHP

---
+++ index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="Cocacola es la bebida">
+	<meta name="keywords" content="bebida, gaseos, cola">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>
+		Inicio - Coca cola
+	</title>
+	<link rel="shortcut icon" href="img/favicon.png">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<!--<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>-->
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,500;0,700;0,900;1,100&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://kit.fontawesome.com/45f2bb29fa.js" crossorigin="anonymous"></script>
+</head>
+<body>
+	<header>
+		<div class="container">
+			<a href="index.html">
+				<img src="img/logo.svg" class="logo">
+			</a>
+			<nav>
+				<a href="#inicio">Inicio</a>
+				<a href="#nosotros">Nosotros</a>
+				<a href="#servicios">Servicios</a>
+				<a href="#galeria">Galería</a>
+				<a href="#contactenos">Contáctenos</a>
+			</nav>
+			<a href="#" class="hamb"><i class="fa-solid fa-bars"></i></a>
+		</div>
+	</header>
+	<main>
+		<section id="inicio">
+			<img src="img/bannerprincipal.jpg">
+			<div class="bloque-inicio">
+				<h1>Bienvenodos a Coca Cola</h1>
+				<p>
+					Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
+				</p>
+				<a href="#nosotros" class="boton boton-rojo">Ver más</a>
+			</div>
+		</section>
+		<section id="nosotros" class="seccion">
+			<div class="container">
+				<p>
+					Un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño.
+			    </p>
+			</div>
+		</section>
+		<section id="servicios" class="seccion">
+			<div class="container">
+			<div class="row">
+				<div class="columna columna-33 columna-mobile-100">
+					<div class="bloque-servicio">
+						<div class="bloque-img-servicio cuadrado-perfecto">
+							<img src="img/servicio1.jpg">
+						</div>
+						<div class="bloque-contenido-servicio">
+							<h3>Servicio 1</h3>
+							<p>
+								Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. 
+								El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". 
+								Estos textos hacen parecerlo un español que se puede leer.
+							</p>
+								<a href="#" class="boton boton-blanco">Ver más</a>
+						</div>
+					</div>
+				</div>
+				<div class="columna columna-33 columna-mobile-100">
+					<div class="bloque-servicio">
+						<div class="bloque-img-servicio cuadrado-perfecto">
+							<img src="img/servicio2.jpg">
+						</div>
+						<div class="bloque-contenido-servicio">
+							<h3>Servicio 1</h3>
+							<p>
+								Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. 
+								El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". 
+								Estos textos hacen parecerlo un español que se puede leer.
+							</p>
+								<a href="#" class="boton boton-blanco">Ver más</a>
+								
+						</div>
+					</div>
+				</div>
+				<div class="columna columna-33 columna-mobile-100">
+					<div class="bloque-servicio">
+						<div class="bloque-img-servicio cuadrado-perfecto">
+							<img src="img/servicio3.jpg">
+						</div>
+						<div class="bloque-contenido-servicio">
+							<h3>Servicio 1</h3>
+							<p>
+								Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. 
+								El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". 
+								Estos textos hacen parecerlo un español que se puede leer.
+							</p>
+								<a href="#" class="boton boton-blanco">Ver más</a>
+						</div>
+				    </div>
+			    </div>
+			</div>
+		</div>
+	   </section>
+		<section id="galeria">
+			<div  class="container-fluid">
+				<div class="row">
+					<div class="columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio1.jpg">
+							<h4>Imagen 1</h4>
+						</div>
+					</div>
+					<div class="columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio2.jpg">
+							<h4>Imagen 2</h4>
+						</div>
+					</div>
+					<div class="columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio3.jpg">
+							<h4>Imagen 3</h4>
+						</div>
+					</div>
+					<div class="columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio4.jpg">
+							<h4>Imagen 4</h4>
+						</div>
+					</div>
+					<div class="columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio5.jpg">
+							<h4>Imagen 5</h4>
+						</div>
+					</div>
+					<div class="columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio6.jpg">
+							<h4>Imagen 6</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="contactenos" class="seccion">
+			<iframe width="523" height="403" frameborder="0" src="https://maps.google.com/maps?width=523&amp;height=403&amp;hl=en&amp;q=6%20avenida%205-84%20zona%201%20Chimaltenango+(guatemala)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+			<div class="container-fluid" >
+				<div class="row">
+					<div class="columna columna-41 columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
+						<form action="index.php" method="post">
+							<div class="form-block">
+								<input type="text" name="nombre" class="form-control" placeholder="Nombre">
+							</div>
+							<div class="form-block">
+								<input type="email" name="email" class="form-control" placeholder="usuario@gmail.com">
+							</div>
+							<div class="form-block">
+								<textarea name="mensaje" placeholder="Mensaje"></textarea>
+							</div>
+							<div class="form-block bloque-ultimo">
+								<input type="submit" class="boton boton-negro" value="enviar">
+							</div>
+							<?php 
+								if($_SERVER["REQUEST_METODO"] == "POST")
+								{
+									$nombre = $_POST["nombre"] ;
+									$email = $_POST["email"] ;
+									$mensaje = $_POST["mensaje"] ;
+
+									if(isset($nombre))
+									{
+										if(isset($email))
+										{
+											if(isset($mensaje))
+											{
+												$para = "micorreo@gmail.com";
+												$asunto = "Esto es una prueba";
+												$cuerpo = $nombre."\n".$email."\n".$mensaje;
+												$adicional = "From: noresponder@correo.com";
+
+												mail($para, $asunto, $cuerpo, $adicional);
+											?>
+												<p>Envio exitoso</p>
+												<?php
+											}
+										}
+									}
+								}
+							?>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="columna columna-25 columna-mobile-100">
+					<img src="img/logo-blanco.png" class="logo-footer">
+					<p>
+						Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño.
+					</p>
+				</div>
+				<div class="columna columna-25 columna-mobile-100">
+					<h3>
+						Temas relacionados
+					</h3>
+					<ul>
+						<li><a href="#">Tema 1</a></li>
+						<li><a href="#">Tema 2</a></li>
+						<li><a href="#">Tema 3</a></li>
+					</ul>
+				</div>
+				<div class="columna columna-25 columna-mobile-100">
+					<h3>
+						Datos de contacto
+					</h3>
+					<ul>
+						<li>geoffreey.torres64@outlook.com</li>
+						<li>+502 48410140</li>
+						<li>Guatemala<br>Guatemala</li>
+					</ul>
+				</div>
+				<div class="columna columna-25 columna-mobile-100">
+					<h3>
+						Redes sociales
+					</h3>
+					<ul class="redes">
+						<li>
+							<a href="#">
+								<i class="fa-brands fa-facebook fa-xl"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="fa-brands fa-x-twitter fa-xl"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="fa-brands fa-square-instagram fa-xl"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="barra-footer">
+			&copy; Derechos Reservados -2023
+		</div>
+	</footer>
+	<script src="js/jquery.js"></script>
+	<script src="js/funciones.js"></script>
+</body>
+</html>
