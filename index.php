7f5d3a94592a367f64c47081a24b1e29a18fdedc
Geoffreey <geoffrey.torres64@gmail.com>
2023-09-17 07:38:35 -0600

    Se modifico lina 13, se retia como comentario

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	</title>
 	<link rel="shortcut icon" href="img/favicon.png">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<!--<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>-->
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,500;0,700;0,900;1,100&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="style.css">
