1c9ef8d5fa909140ec51d7cca7677097f230a176
Geoffreey Cali <geoffrey.torres64@gmail.com>
2024-07-02 14:33:12 -0600

    Se agrega propiedad cursor: pointer para identificar que el boton es un enlace

--- index.php
+++ index.php
@@ -165,7 +165,7 @@
 								<a href="https://wa.link/xj16ys"  class="boton boton-blanco">Demo</a>
 								<a href="http://doc.geoffdevops.com/"  class="boton boton-blanco">Vista</a>
 							</span>
-							<a class="boton boton-azul" id="hideText_btn">ver mas</a>
+							<a class="boton boton-azul" id="hideText_btn" style="cursor: pointer;">ver mas</a>
 						</div>
 				    </div>
 			    </div>
