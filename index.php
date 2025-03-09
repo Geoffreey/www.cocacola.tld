2c8a30c8425e84fbe4b1c810eee503e13db9cf34
Geoffreey Cali <geoffrey.torres64@gmail.com>
2025-03-08 21:02:39 -0600

    Se modifica vista responciva para mejor vista

--- index.php
+++ index.php
@@ -33,13 +33,13 @@
 		</div>
 	</header>
 	<main>
-	<section id="inicio">
-    <div class="bloque-inicio">
-        <h1>Bienvenidos a GeoffDeep</h1>
-        <p>Soporte IT diseñado para satisfacer las necesidades de cada empresa de manera personalizada.</p>
-        <a href="#nosotros" class="boton boton-rojo">Ver más</a>
-    </div>
-</section>
+		<section id="inicio">
+    		<div class="bloque-inicio">
+        		<h1>Bienvenidos a GeoffDeep</h1>
+        		<p>Soporte IT diseñado para satisfacer las necesidades de cada empresa de manera personalizada.</p>
+        		<a href="#nosotros" class="boton boton-rojo">Ver más</a>
+    		</div>
+		</section>
 		<section id="nosotros" class="seccion">
 			<div class="container">
 				<p>
